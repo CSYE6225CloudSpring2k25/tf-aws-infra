8bcc2cbd7d8f45acc46ff33c034403ab8b16609c
iyashsaraf <saraf.y@northeastern.edu>
2025-03-19 03:44:33 -0400

    updated terraform code to create aws resources like S3 Storage for storing fils uploaded, RDS Database instance to store data, security grop for database, added User Data in EC2 Instance and some other required resources

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,11 @@ output "internet_gateway_id" {
 output "app_instance_public_ip" {
   value = aws_instance.app_instance.public_ip
 }
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.endpoint
+}
+
+output "s3_bucket" {
+  value = aws_s3_bucket.bucket.bucket
+}
