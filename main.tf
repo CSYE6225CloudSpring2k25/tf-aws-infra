95dd335311ada161377a3e3c947bb8c29c828327
iyashsaraf <saraf.y@northeastern.edu>
2025-04-02 04:08:34 -0400

    formatted the code

--- main.tf
+++ main.tf
@@ -159,9 +159,9 @@ resource "aws_security_group" "app_sg" {
   description = "Security group for web application"
   vpc_id      = aws_vpc.main.id
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     # cidr_blocks = ["0.0.0.0/0"]
     security_groups = [aws_security_group.lb_sg.id]
   }
